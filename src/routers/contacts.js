c480a054a0e37b22ed91822b073c9fb13a9b27a8
Max Rebryk <rebryk2003@gmail.com>
2024-08-13 18:17:18 +0300

    step 1, 2

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -8,6 +8,9 @@ import {
   patchContactContorller,
   upsertContactController,
 } from '../controllers/contacts.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import { createContactSchema } from '../validation/contacts.js';
+import { isValidId } from '../middlewares/isValidId.js';
 
 const router = Router();
 
@@ -15,12 +18,34 @@ export default router;
 
 router.get('/contacts', ctrlWrapper(getAllContactsController));
 
-router.get('/contacts/:contactId', ctrlWrapper(getContactByIdController));
+router.get(
+  '/contacts/:contactId',
+  isValidId,
+  ctrlWrapper(getContactByIdController),
+);
 
-router.post('/contacts', ctrlWrapper(createContactController));
+router.post(
+  '/contacts',
+  validateBody(createContactSchema),
+  ctrlWrapper(createContactController),
+);
 
-router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
+router.delete(
+  '/contacts/:contactId',
+  isValidId,
+  ctrlWrapper(deleteContactController),
+);
 
-router.put('/contacts/:contactId', ctrlWrapper(upsertContactController));
+router.put(
+  '/contacts/:contactId',
+  isValidId,
+  validateBody(createContactSchema),
+  ctrlWrapper(upsertContactController),
+);
 
-router.patch('/contacts/:contactId', ctrlWrapper(patchContactContorller));
+router.patch(
+  '/contacts/:contactId',
+  isValidId,
+  validateBody(createContactSchema),
+  ctrlWrapper(patchContactContorller),
+);
--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -1,24 +1,26 @@
 import Joi from 'joi';
 
 export const createContactSchema = Joi.object({
-  name: Joi.string().min(3).max(30).required().messages({
+  name: Joi.string().min(3).max(20).required().messages({
     'string.base': 'Username should be a string',
     'string.min': 'Username should have at least {#limit} characters',
     'string.max': 'Username should have at most {#limit} characters',
     'any.required': 'Username is required',
   }),
+  phoneNumber: Joi.string().min(10).max(13).required().messages({
+    'string.min': 'Phone Number should have at least {#limit} characters',
+    'string.max': 'Phone Number should have at most {#limit} characters',
+    'any.required': 'Phone Number is required',
+  }),
   email: Joi.string().email().required(),
-  age: Joi.number().integer().min(6).max(16).required(),
-  gender: Joi.string().valid('male', 'female', 'other').required(),
-  avgMark: Joi.number().min(2).max(12).required(),
-  onDuty: Joi.boolean(),
+  isFavourite: Joi.boolean().required(),
+  contactType: Joi.string().valid('home', 'personal').required(),
 });
 
 export const updateStudentSchema = Joi.object({
-  name: Joi.string().min(3).max(30),
+  name: Joi.string().min(3).max(20),
   email: Joi.string().email(),
-  age: Joi.number().integer().min(6).max(16),
-  gender: Joi.string().valid('male', 'female', 'other'),
-  avgMark: Joi.number().min(2).max(12),
-  onDuty: Joi.boolean(),
+  phoneNumber: Joi.string().min(10).max(13).required(),
+  isFavourite: Joi.boolean().required(),
+  contactType: Joi.string().valid('home', 'personal').required(),
 });
